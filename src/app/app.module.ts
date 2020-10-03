d04b763232acbf02ffd3217d934729c8433f5fe8
Yousef Nafaa <nafaa.youssef93@gmail.com>
2020-10-04 00:05:13 +0200

    premier commit de l'application front Ecommerce

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import {ProductService} from './services/product.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent 
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+    BrowserModule,   
+    AppRoutingModule,
+    HttpClientModule
+  ], 
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
